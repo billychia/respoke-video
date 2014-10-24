7a85775fdd9f5ea42e69c430114834cc8cce5cc8
Billy Chia <billy@billychia.com>
2014-10-24 14:12:36 -0700

    fixed twitter - again maybe

--- app.js
+++ app.js
@@ -219,6 +219,7 @@ var ui = {
             var html = '<a href="https://twitter.com/intent/tweet?button_hashtag=AstriCon2014&text='+ tweetText + '"class="twitter-hashtag-button" id="tweet" data-related="billychia">Tweet #AstriCon</a>';
             var script = "<script id='tweetScript'>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
             $("#controls").append(html).append(script);
+            console.log('just a hack');
         }
 
         if(!$("#tweet").length) {
